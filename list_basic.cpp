aba668a51fc51be59e5c81f6746e91dbfcea34e6
Diogo Pedro <dpfs@cin.ufpe.br>
2018-08-16 06:22:58 -0300

    Add_na_lista

--- list_basic.cpp
+++ list_basic.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 using namespace std;
 
-no *criar_no(int x);
+struct Node{
+    int val;
+    struct Node *pont;
+};
+typedef struct Node node;
+
+node *criar_no(int x);
+void adicionar_lista(int x, node *head);
 /*
-void adicionar_lista();
 void procurar_lista();
 void remocao_lista();
 void permutar_lista();
 */
 
-struct Node{
-	int valor;
-	struct Node *pont;
-};
-typedef struct Node node;
-
 int main(){
-	node *novo;
-	novo = new node();
-	novo->pont = NULL;
-	novo->valor = x;
+    node *head = new node();
 
-	return novo;
+    adicionar_lista(0, head);
+    adicionar_lista(1, head);
+    adicionar_lista(2, head);
+    adicionar_lista(3, head);
 
+    cout << "O valor das listas sao " << head->val << endl;
+    head= head->pont;
+
+    cout << "O valor das listas sao " << head->val << endl;
+    head= head->pont;
+
+    cout << "O valor das listas sao " << head->val << endl;
+    head= head->pont;
+
+    cout << "O valor das listas sao " << head->val << endl;
+    head= head->pont;
+
+    cout << "aguardo lixo de memoria " << head->val << endl;
+    head= head->pont;
+
+    return 0;
 }
 
-no *criar_no(int x){
-	node *novo;
-	novo = new node();
-	novo->pont = NULL;
-	novo->valor = x;
+node *criar_no(int x){//Faz uma alocação dinamica de um novo node,
+    node *novo;		//E retorna o endereço desse node;
+    novo = new node();
+    novo->pont = NULL;
+    novo->val = x;
 
-	return novo;
+    return novo;
 }
+
+void adicionar_lista(int x, node *head){
+    node *el_novo = criar_no(x);
+    node *percussor->pont = head->pont;
+
+    while(percussor->pont != NULL){
+        percussor = percussor->pont;
+    }
+
+    el_novo->pont = percussor->pont;
+}
+
