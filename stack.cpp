0515edee4c037d51b37e7d0f5439ad71947c9acf
Diogo Pedro de Franca Silva <dpfs@ug1c19.windows.cin.ufpe.br>
2018-08-22 11:20:33 -0300

    final funfando

--- stack.cpp
+++ stack.cpp
@@ -10,16 +10,29 @@ typedef struct Node node;
 node *adicionar_lista(int x, node *head);
 void observar_lista(node *head);
 node *remocao_lista(node *head);
+int enfileirar(int vezes);
 
 int main(){
+    int vezes, j;
+    cin >> vezes;
+
+    while(vezes != 0) {
+        j = enfileirar(vezes);
+        if(j == 0){
+            cout << "no" << endl;
+        } else {cout << "yes" << endl;}
+        cin >> vezes;
+    }
+    
+}
+
+int enfileirar(int vezes){
     node *head = new node;
     head->pont = NULL;
     head->val = -1;
 
-    int vezes, numero, vetor, *solucao, i, j;
-
-    cin >> vezes;
-    solucao = new int[vezes];
+    int numero, vetor, *solucao, i, j;
+    solucao = new int[vezes+5];
 
     for(i=0, j=0; i<vezes; i++) {
         cin >> vetor;
@@ -30,10 +43,10 @@ int main(){
         if (vetor > head->val) {
             //cout << "vetor: " << vetor << endl;
             
-            while (vetor >= head->val) {
-                if(head->pont->val == -1){ // Possivel erro;
-                    cout << "Encontrou o vazio" << endl;
+            while (vetor > head->val) {
+                if(head->val == (-1)){ // Possivel erro;
                     break;
+                    //cout << "Encontrou o vazio" << endl;
                 }
 
                 //cout << "Tirando da pilha: " << head->val << endl;
@@ -52,7 +65,7 @@ int main(){
 
         if (i == (vezes-1)) {//Condição para esvaziar a pilha, sem novos elementos p add;
             //cout << "Esvaziando" << endl;
-            while (head->pont != NULL) {
+            while (head->val != (-1)) {
                 //cout << "valor do el com ==: " << head->val << endl;
                 solucao[j] = head->val;
                 head = remocao_lista(head);
@@ -62,10 +75,16 @@ int main(){
 
     }
 
-    for(i=0; i<vezes; i++){
-        cout << solucao[i] << " ";
-    } cout << endl;
-    return 0;
+    for(i=0, j=1; i<(vezes-1); i++){
+        if(solucao[i] > solucao[i+1]){
+            j = 0; 
+            break;
+        }
+    }
+
+    delete head;
+    delete solucao;
+    return j;
 }
 
 node *adicionar_lista(int x, node *head){
@@ -88,15 +107,9 @@ void observar_lista(node *head){
 
 node *remocao_lista(node *head){
     node *auxiliar;
-    if(head == NULL){ //tentando apagar nada, da erro;
-        return NULL;
-    }
-    
-    if(head->pont == NULL){ //sou o ultimo elementod a pilha;
-        delete head;
-        return NULL;
-
-    }else{
+    if(head->val == -1){ //tentando apagar nada;
+        return head;
+    } else{
         auxiliar = head->pont;
         delete head;
         return auxiliar;
