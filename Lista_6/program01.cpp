9f5a214f4af9ca0ac7aa8d3cd70b85f86c47f134
Diogo Pedro de Franca Silva <dpfs@ug4c36.windows.cin.ufpe.br>
2018-11-12 10:30:11 -0300

    test

---
+++ Lista_6/program01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void addGrafo(int edges);
+void findGrafo(int origem);
+
+int main(){
+    int cases, vertices, edges;
+    cin >> cases;
+
+    for(int i=0; i<cases; i++)
+        cin >> vertices >> edges;
+        int origem = addGrafo(edges);
+        findGrafo(origem);
+    }
+
+
+    return 0;
+}
+
+void addGrafo(int edges){
+    int a, b, size;
+    for(int i=0; i<edges; i++){
+
+
+
+
+
+
+    }    
+
+}
+
+void findGrafo(int origem){
+
+
+
+}
